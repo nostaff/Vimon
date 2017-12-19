70a3df870132025503720bc0814d7aa2d9a94215
Eric <nostaff@sina.com>
2017-12-19 23:23:10 +0800

    perf: toast 组件重构优化

--- src/components/toast/toast.vue
+++ src/components/toast/toast.vue
@@ -1,126 +1,163 @@
 <template>
   <div role="dialog" :class="['ion-toast', modeClass, cssClass]" :style="{'z-index': zIndex}">
-    <transition :name="'toast-fade-'+position">
-      <div class="toast-wrapper" :class="['toast-'+position]" v-show="activated">
+    <transition :name="'toast-fade-'+position"
+        @before-enter="beforeEnter"
+        @after-enter="afterEnter"
+        @before-leave="beforeLeave"
+        @after-leave="afterLeave">
+      <div class="toast-wrapper" :class="['toast-'+position]" v-show="isActive">
         <div class="toast-container">
           <div class="toast-message" v-if="message">{{message}}</div>
-          <vm-button class="toast-button" clear v-if="showCloseButton" @click.native="closeButtonClick()">{{closeButtonText}}</vm-button>
+          <vm-button class="toast-button" clear v-if="showCloseButton" @click="closeButtonClick()">{{closeButtonText}}</vm-button>
         </div>
       </div>
     </transition>
   </div>
 </template>
 <script>
-  import {urlChange} from '../../util/dom'
-  import {isFunction, isNumber, isTrueProperty} from '../../util/util'
-  import objectAssign from 'object-assign'
-  import ModeMixins from '../../themes/theme.mixins'
-  import VmButton from '../button'
+import {urlChange, focusOutActiveElement} from '../../util/dom'
+import {isFunction, isNumber, isTrueProperty} from '../../util/util'
+import objectAssign from 'object-assign'
+import ModeMixins from '../../themes/theme.mixins'
+import VmButton from '../button'
 
-  let zIndex = 20001
-  export default {
-    name: 'vm-toast',
-    mixins: [ModeMixins],
-    components: {
-      VmButton
-    },
-    data () {
-      return {
-        defaultOptions: {
-          message: '',
-          closeButtonText: 'OK',
-          showCloseButton: false,
-          dismissOnPageChange: true,
-          duration: 3000,
-          position: 'bottom', // "top", "middle", "bottom"
-          cssClass: '',
-          onDismiss: () => {
-          }
-        },
-
+let zIndex = 20001
+export default {
+  name: 'vm-toast',
+  mixins: [ModeMixins],
+  components: {
+    VmButton
+  },
+  data () {
+    return {
+      defaultOptions: {
         message: '',
         closeButtonText: 'OK',
         showCloseButton: false,
         dismissOnPageChange: true,
         duration: 3000,
-        position: 'bottom',
+        position: 'bottom', // "top", "middle", "bottom"
         cssClass: '',
-        onDismiss: () => {
-        },
-
-        activated: false,
-        timer: null,
-
-        zIndex: this.getZIndex()
-      }
-    },
-    created () {
-      if (this.dismissOnPageChange) {
-        urlChange(() => {
-          if (this.activated) {
-            if (this.showCloseButton) {
-              this.closeButtonClick()
-            } else if (this.timer) {
-              window.clearTimeout(this.timer)
-              this.timer = null
-              this.dismiss(-1)
-            }
-          }
-        })
-      }
-    },
-
-    methods: {
-      getZIndex () {
-        return zIndex++
+        onDismiss: () => {}
       },
-      present (options) {
-        let _options = objectAssign({}, this.defaultOptions, options)
-        this.message = _options.message
-        this.closeButtonText = _options.closeButtonText
-        this.cssClass = _options.cssClass
-        this.showCloseButton = isTrueProperty(_options.showCloseButton)
-        this.dismissOnPageChange = isTrueProperty(_options.dismissOnPageChange)
-        if (['top', 'middle', 'bottom'].indexOf(_options.position) > -1) {
-          this.position = _options.position
-        }
-        if (isNumber(_options.duration)) {
-          this.duration = _options.duration
-        }
-        if (isFunction(_options.onDismiss)) {
-          this.onDismiss = _options.onDismiss
-        }
 
-        this.activated = true
-        if (!this.showCloseButton) {
-          this.timer = window.setTimeout(() => {
+      message: '',
+      closeButtonText: 'OK',
+      showCloseButton: false,
+      dismissOnPageChange: true,
+      duration: 3000,
+      position: 'bottom',
+      cssClass: '',
+      onDismiss: () => {},
+
+      isActive: false,
+      timer: null,
+
+      zIndex: this.getZIndex()
+    }
+  },
+  created () {
+    if (this.dismissOnPageChange) {
+      this.unReg = urlChange(() => {
+        if (this.isActive) {
+          if (this.showCloseButton) {
+            this.closeButtonClick()
+          } else if (this.timer) {
+            window.clearTimeout(this.timer)
             this.timer = null
-            this.dismiss(-1)
-          }, this.duration)
+            this.dismiss()
+          }
         }
+      })
+    }
+  },
 
-        return new Promise((resolve, reject) => {
-          this.$on('onDismissEvent', (res) => {
-            resolve(res)
-          })
-        })
-      },
+  methods: {
+    beforeEnter () {
+      this.enabled = false // 不允许过渡中途操作
+      this.$app && this.$app.setEnabled(false, 400)
+    },
+    afterEnter () {
+      this.presentCallback()
+      focusOutActiveElement()
+      let focusableEle = document.querySelector('button')
+      if (focusableEle) {
+        focusableEle.focus()
+      }
+      this.enabled = true
+    },
+    beforeLeave () {
+      this.enabled = false
+      this.$app && this.$app.setEnabled(false, 400)
+    },
+    afterLeave () {
+      this.dismissCallback()
+      // 删除DOM
+      this.$el.remove()
+      this.enabled = true
+    },
+    getZIndex () {
+      return zIndex++
+    },
+    present (options) {
+      let _options = objectAssign({}, this.defaultOptions, options)
+      this.message = _options.message
+      this.closeButtonText = _options.closeButtonText
+      this.cssClass = _options.cssClass
+      this.showCloseButton = isTrueProperty(_options.showCloseButton)
+      this.dismissOnPageChange = isTrueProperty(_options.dismissOnPageChange)
+      if (['top', 'middle', 'bottom'].indexOf(_options.position) > -1) {
+        this.position = _options.position
+      }
+      if (isNumber(_options.duration)) {
+        this.duration = _options.duration
+      }
+      if (isFunction(_options.onDismiss)) {
+        this.onDismiss = _options.onDismiss
+      }
 
-      dismiss (role) {
+      this.isActive = true
+      if (!this.showCloseButton) {
+        this.timer = window.setTimeout(() => {
+          this.timer = null
+          this.dismiss()
+        }, this.duration)
+      }
+
+      this.isActive = true
+      return new Promise((resolve) => {
+        this.presentCallback = resolve
+      })
+    },
+
+    dismiss () {
+      if (this.isActive) {
+        this.isActive = false
         this.onDismiss && this.onDismiss(role)
 
-        this.$nextTick(() => {
-          this.$emit('onDismissEvent', role)
-
-          this.$el.remove()
+        this.dismissOnPageChange && this.unReg && this.unReg()
+        if (!this.enabled) {
+          this.$nextTick(() => {
+            this.dismissCallback()
+            this.$el.remove()
+            this.enabled = true
+          })
+        }
+        return new Promise((resolve) => {
+          this.dismissCallback = resolve
         })
-      },
-
-      closeButtonClick () {
-        this.dismiss(-1)
+      } else {
+        return new Promise((resolve) => {
+          resolve()
+        })
       }
+    },
+
+    closeButtonClick () {
+      this.dismiss()
     }
   }
+}
 </script>
 
 <style lang="scss">
