e82b0f1e268ceaad98030058c470aa4b28ca7104
Eric <nostaff@sina.com>
2017-12-23 00:36:57 +0800

    perf: segment 组件重构优化

--- src/components/segment-button/segment-button.vue
+++ src/components/segment-button/segment-button.vue
@@ -2,30 +2,14 @@
   <div class="segment-button" role="button" @click="onClickHandler"
        :aria-pressed="isActive"
        :class="{
-            'segment-activated':isActive,
-            'segment-button-disabled':isDisabled
+          'segment-activated':isActive,
+          'segment-button-disabled':isDisabled
         }">
     <slot></slot>
     <div class="button-effect"></div>
   </div>
 </template>
 <script type="text/javascript">
-/**
- * @component Segment/SegmentButton
- * @description
- *
- * ## 小标签 / SegmentButton
- *
- * Segment组件的子组件SegmentButton, 两者配合使用, 属于嵌套关系.
- *
- * @props {String|Number} value - 当前SegmentButton的值, 如果父元素的value和这个相同, 这个当前被选中
- * @props {Boolean} [disabled=false] - 当前SegmentButton的禁用状态
- *
- * @slot 空 - 当前button的显示值, 如果没有提供value值, 建议不要嵌套过多的结构.
- *
- * @fires component:Segment#onSelect
- * @see component:Segment
- */
 import {isTrueProperty, isPresent} from '../../util/util'
 import ModeMixins from '../../themes/theme.mixins'
 
@@ -35,7 +19,12 @@ export default {
   inject: {
     segmentComponent: {
       from: 'segmentComponent',
-      default: null
+      default () {
+        if (process.env.NODE_ENV !== 'production') {
+          console.error('[Component] SegmentButton 需要与 Segment组件 组合使用!')
+        }
+        return null
+      }
     }
   },
   props: {
