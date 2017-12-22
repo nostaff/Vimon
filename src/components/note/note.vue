73836b8c0a9044232b31eb17e9aea275b471b5df
Eric <nostaff@sina.com>
2017-12-23 00:38:29 +0800

    perf: note 组件重构优化

--- src/components/note/note.vue
+++ src/components/note/note.vue
@@ -1,20 +1,19 @@
 <template>
-    <span class="ion-note" :class="[modeClass]">
-        <slot></slot>
-    </span>
+  <span class="ion-note" :class="[modeClass]">
+    <slot></slot>
+  </span>
 </template>
 
 <script>
-  import ModeMixins from '../../themes/theme.mixins'
+import ModeMixins from '../../themes/theme.mixins'
 
-  export default {
-    name: 'vm-note',
-    mixins: [ModeMixins]
-  }
+export default {
+  name: 'vm-note',
+  mixins: [ModeMixins]
+}
 </script>
 
-
 <style lang="scss">
-  @import 'note.ios';
-  @import 'note.md';
+@import "note.ios";
+@import "note.md";
 </style>
