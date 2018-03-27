14fa9e68ac84fc5d01e8221afcabdaad08902921
Eric <nostaff@sina.com>
2018-03-27 18:21:36 +0800

    feat: Badge 组件增加圆点属性

--- src/components/badge/badge.vue
+++ src/components/badge/badge.vue
@@ -1,20 +1,20 @@
 <template>
-    <span class="ion-badge" :class="[modeClass, colorClass]">
+    <sup class="ion-badge" :class="[modeClass, colorClass]">
         <slot></slot>
-    </span>
+    </sup>
 </template>
 
 <script type="text/javascript">
-  import ModeMixins from '../../themes/theme.mixins'
+import ModeMixins from '../../themes/theme.mixins'
 
-  export default {
-    name: 'vm-badge',
-    mixins: [ModeMixins]
-  }
+export default {
+  name: 'vm-badge',
+  mixins: [ModeMixins]
+}
 </script>
 
 <style lang="scss">
-  @import 'badge';
-  @import 'badge.ios';
-  @import 'badge.md';
+@import "badge";
+@import "badge.ios";
+@import "badge.md";
 </style>
