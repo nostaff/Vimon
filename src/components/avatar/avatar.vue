2c9d8c301034cc8c5fbd534fd697a78b84197d28
Eric <nostaff@sina.com>
2018-03-29 21:33:37 +0800

    feat: Avatar组件改为容器，slot可以是图片、图标、文字，并支持tile属性控制形状为圆形或正方形

--- src/components/avatar/avatar.vue
+++ src/components/avatar/avatar.vue
@@ -1,17 +1,57 @@
 <template>
-    <div class="ion-avatar">
-        <img :src="src" v-if="src">
-        <slot></slot>
-    </div>
+  <div class="ion-avatar" :class="[colorClass]" :style="style">
+    <slot></slot>
+  </div>
 </template>
+
 <script type="text/javascript">
-    export default {
-      name: 'vm-avatar',
-      props: {
-        src: {
-          type: String,
-          default: ''
-        }
+import ModeMixins from '../../themes/theme.mixins'
+import { isTrueProperty } from '../../util/util'
+export default {
+  name: 'vm-avatar',
+  mixins: [ModeMixins],
+  props: {
+    size: {
+      type: [Number, String],
+      default: 36
+    },
+    tile: [Boolean, String]
+  },
+  data () {
+    return {
+      isTile: isTrueProperty(this.tile)
+    }
+  },
+  computed: {
+    style () {
+      return {
+        width: parseInt(this.size) + 'px',
+        height: parseInt(this.size) + 'px'
       }
     }
+  },
+  mounted () {
+    if (this.isTile) { this.setElementClass('avatar-tile') }
+  }
+}
 </script>
+
+<style lang="scss">
+  .ion-avatar {
+    border-radius: 50%;
+    text-align: center;
+    vertical-align: middle;
+
+    img, .icon {
+        border-radius: 50%;
+    }
+
+    &.avatar-tile {
+      border-radius: 0;
+
+      img, .icon {
+          border-radius: 0;
+      }
+    }
+  }
+</style>
--- src/components/avatar/index.js
+++ src/components/avatar/index.js
@@ -1,1 +1,36 @@
+/**
+ * @component Avatar
+ * @description
+ *
+ * ## 其他 / Avatar 头像
+ *
+ * 基础的头像组件, 可以设置大小、颜色等.
+ *
+ * ### 如何引入
+ * ```
+ * // 引入
+ * import { Avatar } from 'vimon'
+ * // 安装
+ * Vue.component(Avatar.name, Avatar)
+ * // 或者
+ * export default{
+ *   components: {
+ *    Avatar
+ *  }
+ * }
+ * ```
+ *
+ * @props {String} [color='default'] - 颜色
+ * @props {Number} [size]       - 尺寸
+ * @props {Boolean} [tile]      - 是否方形边框，默认为否，圆形
+ *
+ * @demo #/list
+ *
+ * @slot 空     头像内容，可以是图片、图标、文字等
+ *
+ * @usage
+ * <Avatar><img src="/static/avatar-1.png"/></Avatar>
+ * <Avatar color="danger" size="48"><Icon name="heart"></Icon></Avatar>
+ */
+
 export { default } from './avatar.vue'
